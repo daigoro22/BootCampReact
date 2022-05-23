eb000df76d4c60145cdc919e534218f8ebb6c464
daigoro <daigoro.abe@systemi.co.jp>
2022-05-23 11:48:36 +0900

    Modified App.jsx
    close #1 className による動的なボタンのスタイルの切り替え

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,23 @@ import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
-{// import Button from 'react-bootstrap/Button'
-}
+import classnames from "classnames"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0) // js の分割代入の書き方。現在の state と state を更新する関数をペアにして返す
+
+  const countClassNames = {
+    "btn": true,
+    "btn-outline-success": count < 10,
+    "btn-outline-warning": count > 9 && count < 20,
+    "btn-outline-danger": count > 19,
+    "disabled": count > 29
+  }
 
   return (
     <div className="App">
       <header className="App-header">
-        <button className="btn btn-outline-danger" onClick={() => setCount((count) => count + 1)}>
+        <button className={classnames(countClassNames)} onClick={() => setCount((count) => count + 1)}>
           count is: {count}
         </button>
       </header>
